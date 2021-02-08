6943f06c08a6dd2c72f8234473a9cac5a5f49037
bily101 <lawingvasiliy06@gmail.com>
2021-02-08 09:01:37 -0500

    made the adapter and the broadcast address optional

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import re, uuid
 print ("Your current MAC address: ", end="") 
 print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
     
-os.system("ping 10.57.1.255 -b -c 5")
+adapter= input("Give me your network adapter for example\n wlp5s0 \n: ")
+broadcast = input("I need your broadcast ip for example mine is 10.57.1.255 \n: ")
+
+os.system(f"ping {broadcast} -b -c 5")
 os.system("sudo rm mac.txt")
 os.system("sudo arp -a >> mac.txt")
 
@@ -19,7 +22,6 @@ mac = list(lines[0])
 # 19..35
 x = " "
 y = " "
-z = " " 
 for i in mac[19:35]:
     x += i
 for i in mac[51:70]:
@@ -32,9 +34,9 @@ confirm = input("(y: Yes, n: No): ")
 
 if confirm == "y":
     print("OK changing mac address")
-    os.system("sudo ifconfig wlp5s0 down")
-    os.system(f"sudo ifconfig wlp5s0 hw ether {x}")
-    os.system(f"sudo ifconfig wlp5s0 up")
+    os.system(f"sudo ifconfig {adapter} down")
+    os.system(f"sudo ifconfig {adapter} hw ether {x}")
+    os.system(f"sudo ifconfig {adapter} up")
     print("Finished Your new MAC ADDRESS is" + x +  "\n To change it again please let me know")
     input("Enter to exit out: ")
 	
